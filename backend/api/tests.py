3b2d2608375bf9da8f8dd6ed9bd00150922425a8
NirinaTanjona <tanjonawork@gmail.com>
2023-04-14 08:57:01 +0300

    add BankingApiTestCase class to do some test for the app

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from api.models import User, Transaction
 
-# Create your tests here.
+class BankingApiTestCase(APITestCase):
+    """
+    Test for Banking API
+    """
+
+    def setUp(self):
+
+        self.user1 = User.objects.create_user(
+            username='testuser1',
+            password='this_is_a_test'
+        )
+        self.user2 = User.objects.create_user(
+            username='testuser2',
+            password='this_is_a_test'
+        )
+        self.admin = User.objects.create_superuser(
+            username='admin',
+            password='admin'
+        )
+
+    def test_users_role(self):
+        '''
+        test some users role (check if admin or user)
+        '''
+        self.assertEqual(self.user1.is_superuser, False)
+        self.assertEqual(self.admin.is_superuser, True)
